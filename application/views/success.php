42b5621bfb64a4e14db7446df06948b190e58061
gatpanramadhan <gatpanamadhan21@gmail.com>
2024-06-30 12:27:32 +0700

    Bus Booking

---
+++ application/views/success.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>MORNING PERSON BUS</title>
+   <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/bootstrap.css') ?>">
+   <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/custom.css?date=' . date("s")) ?>">
+   <link rel="shortcut icon" href="assets/frontend/images/biss.png" type="image/x-icon">
+
+</head>
+
+<body style="height:1500px">
+
+   <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+      <a class="navbar-brand" href="#">MORNING PERSON BUS BOOKING </a>
+      <ul class="navbar-nav" style="margin-left:70%">
+         <li class="nav-item active">
+            <a class="nav-link" href="<?php echo base_url('home') ?>">Home</a>
+         </li>
+      </ul>
+   </nav>
+   <div class="container" style="margin-top:100px">
+      <h3 class="text-success">Your Bus Successfully Booked | For More Information Contact Suport Team</h3>
+
+   </div>
+</body>
+
+</html>
